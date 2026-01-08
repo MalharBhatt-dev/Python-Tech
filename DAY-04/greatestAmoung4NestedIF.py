622c5bd8f2ad703202fbea456ea2faa94a2070b9
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-01-08 18:12:08 +0530

    DAY-04 NESTED IF..1

--- DAY-04/greatestAmoung4NestedIF.py
+++ DAY-04/greatestAmoung4NestedIF.py
@@ -6,25 +6,25 @@ b = int(input('Enter the second number:'))
 c = int(input('Enter the third number:'))
 d = int(input('Enter the fourth number:'))
 
-if a>b :
-    if a>c :
-        if a>d :
+if a>=b :
+    if a>=c :
+        if a>=d :
             print(f"{a} is the greatest.")
         else:
             print(f"{d} is the greatest.")
     else:
-        if c>d :
+        if c>=d :
             print(f"{c} is the greatest.")
         else:
             print(f"{d} is the greatest.")
 else:
-    if b>c :
-        if b>d :
+    if b>=c :
+        if b>=d :
             print(f"{b} is the greatest.")
         else:
             print(f"{d} is the greatest.")
     else:
-        if c>d :
+        if c>=d :
             print(f"{c} is the greatest.")
         else:
             print(f"{d} is the greatest.")
