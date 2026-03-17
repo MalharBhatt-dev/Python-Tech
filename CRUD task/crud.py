87f67721bf332b66ed08468075aa7642a90faf78
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-03-17 22:34:54 +0530

    Implement ChangeDepartment Feature.

--- CRUD task/crud.py
+++ CRUD task/crud.py
@@ -1,31 +1,22 @@
 import sqlite3
 from datetime import date
 
-#* -------------------Insert Data-----------------
+#~ -------------------Insert Data-----------------
+Manager = {"SALES": "Mr Rohit", "IT": "Mr Shyam", "HR": "Mr Raj"}
 
 def insert_data(obj):
-    departments = ['HR', 'SALES', 'IT']
 
     insert_main_employee(obj)
 
-    insert_department_employee(obj, departments)
+    insert_department_employee(obj)
 
     search_employee(obj.EMPID)
 
 
-def insert_department_employee(obj, departments):
+def insert_department_employee(obj):
     data = sqlite3.connect('TESTYANTRA.db')
     cursor = data.cursor()
-
-    if obj.DEPARTMENT in departments:
-        table_name = obj.DEPARTMENT.upper()
-
-        cursor.execute(f"INSERT INTO {table_name} VALUES(?,?,?,?,?,?,?)", (obj.EMPID,
-            obj.ENAME, obj.DEPARTMENT, obj.MOBNUMBER, obj.SALARY, obj.JD, obj.Manager  ))
-
-    else:
-        print("INVALID DEPARTMENT")
-
+    cursor.execute(f"INSERT INTO {obj.DEPARTMENT} VALUES(?,?,?,?,?,?,?)", (obj.EMPID,obj.ENAME, obj.DEPARTMENT, obj.MOBNUMBER, obj.SALARY, obj.JD, obj.Manager  ))
     data.commit()
     data.close()
 
@@ -97,9 +88,10 @@ def update_salary(empid, new_salary):
 def delete_employee(empid):
     data = sqlite3.connect('TESTYANTRA.db')
     cursor = data.cursor()
-
-    cursor.execute("DELETE FROM EMPLOYEE WHERE EMPID=?", (empid,))
-
+    cursor.execute(f"select department from employee where empid = {empid}")
+    record = cursor.fetchone()
+    cursor.execute(f"DELETE FROM EMPLOYEE WHERE EMPID = {empid}")
+    cursor.execute(f"delete from {record[0]} where empid = {empid}")
     data.commit()
     data.close()
 
@@ -120,6 +112,27 @@ def department_employee(dept):
 
     data.close()
 
+#~ -----------------------Change Departments----------------
+
+def change_department(empid , new_dept):
+    data = sqlite3.connect('TESTYANTRA.db')
+    cursor = data.cursor()
+    # cursor.execute(f"select * from employee where empid = {empid}") #!SQL Injection May occur using this format.
+    cursor.execute(f"select * from employee where empid = ?",(empid,))
+    record = cursor.fetchone()
+    cursor.execute('update employee set department = ? where empid = ?',(new_dept,empid))
+    cursor.execute(f"select * from employee where empid = ?",(empid,))
+    new_record = list(cursor.fetchone())
+    new_record.append(Manager[new_dept])
+    print(new_record)
+    cursor.execute(f"INSERT INTO {new_dept} (EMPID , ENAME,DEPARTMENT,MOBILENUMBER,SALARY,JD,MANAGER) VALUES (?,?,?,?,?,?,?)",(tuple(new_record)))
+    cursor.execute(f"select * from {new_dept} where empid = ?",(empid,))
+    dept_record = cursor.fetchone()
+    print(dept_record)
+    cursor.execute(f'delete from {record[2]} where empid = ?',(empid,))
+    print("Updation is successful.")
+    data.commit()
+    data.close()
 
 # *--------------------Login-----------------
 
